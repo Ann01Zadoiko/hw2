776f5cf75772c5f6d6abb02fd8de1c23def63930
Ann01Zadoiko <70943933+Ann01Zadoiko@users.noreply.github.com>
2020-09-24 16:20:33 +0300

    Add files via upload

---
+++ Task_01.java
@@ -0,0 +1,151 @@
+package com.zadoiko.homework._03;
+
+public class Task_01 {
+    /* public class Task_01 {
+    //Example #1
+    int chinaPopulation = 1360000000;
+    int indiaPopulation = 1240000000;
+    int total = chinaPopulation + indiaPopulation;
+System.out.println(total); //-1694967296
+    //Example #2
+    byte b1=1;
+    byte b2=2;
+ //   byte b3=b1+b2;  error compilation
+    //Example #3
+    int i = 123456789;
+    float f = i;
+    double d = i;
+    short s = (short) i;
+System.out.println("int: " + i);//123456789
+System.out.println("float: " + f);//1.23456792E8
+System.out.println("double: " + d);//1.23456792E8
+System.out.println("short: " + s);//-13035
+//Example #4
+int a=1;
+    int b=0;
+    int c=a/b;
+System.out.println(c);//error
+//Example #5
+double a=1;
+    double b=0;
+    double c=a/b;
+System.out.println(c);
+System.out.println("c+1 = " + (c + 1));
+System.out.println("+0.0 == -0.0 : " + (0.0 == -0.0));
+System.out.println("a/(+0.0) = " + (a/(+0.0)));
+System.out.println("a/(-0.0) = " + (a/(-0.0)));//error
+//Example #6
+double a=0;
+    double b=0;
+    double c=a/b;
+System.out.println("c =" + c);
+System.out.println("c+0 =" + (c + 0));
+System.out.println("c<0 =" + (c < 0));
+System.out.println("c>0 =" + (c > 0));
+System.out.println("c==0 =" + (c == 0));
+System.out.println("c!=0 =" + (c != 0));
+System.out.println("c==c =" + (c == c)); // :)
+System.out.println("c!=c =" + (c != c)); // :)
+System.out.println("c == NaN: " + (c == Double. NaN));
+System.out.println("c is NaN: " + Double. isNaN(c)); correct
+    //Example #7
+    switch (colorNum) {
+        case 0:
+            setBackground(Color.RED);
+            break;
+        case 1:
+            setBackground(Color.GREEN);
+            break;
+        default:
+            setBackground(Color.BLACK);
+            break;
+    }
+    //the color will be displayed
+    //Example #8
+    for (int i = 0; i < 10; i++) {
+        System.out.println("Hello, World!");
+    }
+for (int i=0, j=10; i<=10; i++, j--){
+        System.out.println(i + " " + j);
+    }
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    0 10
+    1 9
+    2 8
+    3 7
+    4 6
+    5 5
+    6 4
+    7 3
+    8 2
+    9 1
+    10 0
+
+
+//Example #9
+int i=0;
+while (i < 10) {
+        System.out.println("Hello, World!");
+        i++;
+    }
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+//Example #10
+int i=0;
+do {
+        System.out.println("Hello World");
+        i++;
+    } while (i < 10);
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+
+//Example #11
+int i=0;
+while (true) { // eternal cycle
+        System.out.println("Hello, World!" );
+        i++;
+        if (i>5)
+            break; // early exit from the cycle
+    }
+    //Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+    Hello, World!
+//Example #12
+int cs=0;
+//for (int i=1; i<=20; i++) {
+//        if (i == 13)
+ //           continue;
+ //       cs = cs + i;
+           }//skip operation when i=13
+
+}
+  */
+}
